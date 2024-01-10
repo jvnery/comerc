cef647f69e6ee0883818fef19ecadfdb69b5a5d6
João Vitor <88397349+jvnery@users.noreply.github.com>
2024-01-10 16:46:40 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:60,
+    autoplay:true,
+    animateOut: 'fadeOut',
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
